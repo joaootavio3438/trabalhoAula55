93637cd90a5724b72401a2ee9a62c0857897d354
= <=>
2023-05-12 15:15:48 -0300

    trabalhoJoao2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref } from 'vue'
 
-
-// const item = ref('')
-
-const listaProdutos = ref([
+const enviar = ref(false)
+let valorcarrinho = ref(0)
+const carrinho = ref([])
+const produtos = ref([
   {
     id: 1,
     name: 'Camisa',
@@ -43,32 +43,65 @@ const listaProdutos = ref([
   },
 ])
 
-// function adicionar(novoItem) {
-//     itens.value.push(novoItem.value)
-//     novoItem.value = ''
-//   }
-//   function remover (index) {
-//     novoItem.value.splice(index, 1)
-//   }
-
+  function addCarrinho(id, name,preco,quantidade){
+    if(quantidade == 0) {
+      alert('Adicione o produto primeiro antes de colocar no carrinho')
+    } else{carrinho.value.push({id, name,preco,quantidade})}
+  }
+  function limparCarrinho(){
+    carrinho.value = []
+    valorcarrinho.value = 0
+}
+function verCarrinho(){
+  enviar.value = !enviar.value
+}
 </script>
 
 <template>
   
-  
+
   <ul>
-    <li v-for="(item, index) in listaProdutos" :key="item.id"> {{ item }} 
+    <div class="produtos">
+    <li v-for="(item, index) in produtos" :key="index">
       <div>{{ item.name }}</div>
       <div>{{ item.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL'}) }}</div>
+      <div>Quantidade: {{ item.quantidade }}</div>
       <div>
-        <button @click="adicionar">Adicionar</button> 
+        <button @click="item.quantidade++">Adicionar</button>
+      <button @click="item.quantidade--" v-if="item.quantidade > 0">Remover</button>
       </div>
-      <button @click="remover(index)">Remover</button>
-       
+      <div>
+        <button @click="addCarrinho(item.id, item.name, item.preco, item.quantidade)">adicionar ao carrinho</button>
+      </div>
     </li>
+  </div>
   </ul>
+  <button @click="verCarrinho()">ver carrinho</button>
+  <div class="Carrinho" v-if="enviar">
+  <ul>
+      <li v-for="(item,index) in carrinho" :key="index">
+        <div>{{ item.name }}</div>
+        <div>{{ item.preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL'}) }}</div>
+        <div>Quantidade: {{ item.quantidade }}</div>
+      </li>
+      <button @click="limparCarrinho()">Limpar carrinho</button>
+  </ul>
+</div>
 
 </template>
 
 <style scoped>
+  li{
+    list-style: none;
+    margin: 15px 20px;
+  }
+  .produtos{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    width: 450px;  
+  }
+  button{
+    margin: 4px 0;
+  }
 </style>
